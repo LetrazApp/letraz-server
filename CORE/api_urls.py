d5384e33640b050f0b2638075ad632642a43c802
Arnab Biswas <73281048+pingArnab@users.noreply.github.com>
2025-01-06 16:29:18 +0530

    feat: Add error framework (#1)

--- CORE/api_urls.py
+++ CORE/api_urls.py
@@ -3,4 +3,6 @@ from . import api_views
 
 urlpatterns = [
     path('health/', api_views.health_check),
+    path('error-exapmple/', api_views.error_example),
+    path('error-list-exapmple/', api_views.error_list_example),
 ]
--- CORE/api_views.py
+++ CORE/api_views.py
@@ -3,6 +3,8 @@ import logging
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from letraz_server.contrib.constant import ErrorCode
+from letraz_server.contrib.error_framework import ErrorResponse, ErrorResponseList
 from letraz_server.settings import PROJECT_NAME
 
 __module_name = f'{PROJECT_NAME}.' + __name__
@@ -13,3 +15,26 @@ logger = logging.getLogger(__module_name)
 def health_check(request):
     logger.info('Health check: ok')
     return Response({'status': 'ok'})
+
+
+@api_view(['GET'])
+def error_example(request):
+    error_response: ErrorResponse = ErrorResponse(
+        code=ErrorCode.INVALID, message='Example error!',
+        details='Example error details!', extra='Example extra data!'
+    )
+    logger.info(f'UUID -> {error_response.uuid} | Error example was called!')
+    return error_response.response
+
+
+@api_view(['GET'])
+def error_list_example(request):
+    error_list: ErrorResponseList = ErrorResponseList('Multiple error found!')
+    for i in range(1, 5):
+        error_response: ErrorResponse = ErrorResponse(
+            code=ErrorCode.INVALID, message=f'Example error - {i+1}!',
+            details='Example error details!', extra='Example extra data!'
+        )
+        logger.info(f'UUID -> {error_response.uuid} | Error Response List example was called!')
+        error_list.add_error_obj(error_response)
+    return error_list.get_error_list_response()
