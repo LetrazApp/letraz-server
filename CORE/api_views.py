3a7be753a2874272d8227bac5ec351cc3cffbb70
Arnab Biswas <ping.arnab@gmail.com>
2025-01-11 20:47:52 +0530

    feat: Add APIs for create and get all Waitlist

--- CORE/api_views.py
+++ CORE/api_views.py
@@ -1,8 +1,12 @@
 import logging
 
+from django.db.models import QuerySet
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from CORE.models import Waitlist
+from CORE.serializers import WaitlistSerializer
 from letraz_server.contrib.constant import ErrorCode
 from letraz_server.contrib.error_framework import ErrorResponse, ErrorResponseList
 from letraz_server.settings import PROJECT_NAME
@@ -32,9 +36,32 @@ def error_list_example(request):
     error_list: ErrorResponseList = ErrorResponseList('Multiple error found!')
     for i in range(1, 5):
         error_response: ErrorResponse = ErrorResponse(
-            code=ErrorCode.INVALID, message=f'Example error - {i+1}!',
+            code=ErrorCode.INVALID, message=f'Example error - {i + 1}!',
             details='Example error details!', extra='Example extra data!'
         )
         logger.info(f'UUID -> {error_response.uuid} | Error Response List example was called!')
         error_list.add_error_obj(error_response)
     return error_list.get_error_list_response()
+
+
+@api_view(['GET', 'POST'])
+def waitlist_crud(request):
+    try:
+        if request.method == 'GET':
+            waitlist_qs: QuerySet[Waitlist] = Waitlist.objects.all().order_by('waiting_number')
+            return Response(WaitlistSerializer(waitlist_qs, many=True).data)
+        if request.method == 'POST':
+            waitlist_serializer: WaitlistSerializer = WaitlistSerializer(data=request.data)
+            if waitlist_serializer.is_valid():
+                waitlist: Waitlist = waitlist_serializer.save()
+                return Response(WaitlistSerializer(waitlist).data, status=status.HTTP_201_CREATED)
+            else:
+                return ErrorResponse(
+                    code=ErrorCode.INVALID_REQUEST, message=f'Invalid Data provided!',
+                    details=waitlist_serializer.errors, extra={'data': request.data}
+                ).response
+    except Exception as e:
+        error_response = ErrorResponse(code=ErrorCode.INVALID_REQUEST, message=e.__str__(),
+                                       extra={'data': request.data})
+        logger.exception(f'UUID -> {error_response.uuid} | Unknown error encountered: {e.__str__()}')
+        return error_response.response
--- CORE/serializers.py
+++ CORE/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from .models import Country
+from .models import Country, Waitlist
 
 
 class CountrySerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Country
+        fields = '__all__'
+
+
+class WaitlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Waitlist
         fields = '__all__'
