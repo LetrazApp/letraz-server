968c183c11d076e1b7252df551fa04cf458ed9ed
Arnab Biswas <ping.arnab@gmail.com>
2025-01-22 18:08:43 +0530

    chore: update RESUME.Education & RESUME.Experience APIs fetch user from Clerk authentication and authorization and auto resume section creation/deletion feature

--- RESUME/api_views.py
+++ RESUME/api_views.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from CORE.serializers import ErrorSerializer
 from PROFILE.models import User
-from RESUME.models import Resume, Education, Experience
+from RESUME.models import Resume, Education, Experience, ResumeSection
 from RESUME.serializers import ResumeShortSerializer, ResumeFullSerializer, EducationFullSerializer, \
     ExperienceFullSerializer, EducationUpsertSerializer, ExperienceUpsertSerializer
 from letraz_server.contrib.constant import ErrorCode
@@ -161,12 +161,19 @@ def resume_crud(request, resume_id: str | None = None):
     description="Deletes an education from the user's resume as specified in the resume id."
 )
 @api_view(['GET', 'POST', 'DELETE'])
-def education_crud(request, user_id: str, resume_id: str, education_id: str | None = None):
+def education_crud(request, resume_id: str, education_id: str | None = None):
     try:
+        # Ownership Check for all types of API
+        authenticated_user: User = request.user
+        if not authenticated_user.resume_set.filter(id=resume_id).exists():
+            return ErrorResponse(code=ErrorCode.NOT_FOUND, message='Resume not found!', status_code=404).response
+        resume: Resume = authenticated_user.resume_set.get(id=resume_id)
+
+        # Main APIs Logic
         if request.method == 'GET':
             if education_id:
-                education_sq: QuerySet[Education] = Education.objects.filter(
-                    user__id=user_id, resume_section__resume__id=resume_id, id=education_id
+                education_sq: QuerySet[Education] = authenticated_user.education_set.filter(
+                    resume_section__resume=resume, id=education_id
                 )
                 if education_sq.exists():
                     return Response(EducationFullSerializer(education_sq.first()).data)
@@ -176,27 +183,37 @@ def education_crud(request, user_id: str, resume_id: str, education_id: str | No
                     ).response
             else:  # If resume id is not provided send all resumes for the user
                 return Response(
-                    EducationFullSerializer(Education.objects.filter(
-                        user__id=user_id, resume_section__resume__id=resume_id
+                    EducationFullSerializer(authenticated_user.education_set.filter(
+                        resume_section__resume=resume
                     ), many=True).data
                 )
         if request.method == 'POST':
             if education_id:
                 return ErrorResponse(code=ErrorCode.INVALID_REQUEST,
                                      message='Education Id i not allowed in create request!').response
-            education_ser: EducationUpsertSerializer = EducationUpsertSerializer(data=request.data)
+            new_resume_section = resume.create_section(section_type=ResumeSection.ResumeSectionType.Education)
+            payload = request.data
+            payload['user'] = authenticated_user.id
+            payload['resume_section'] = new_resume_section.id
+            education_ser: EducationUpsertSerializer = EducationUpsertSerializer(data=payload)
             if education_ser.is_valid():
                 new_education = education_ser.save()
                 return Response(EducationFullSerializer(new_education).data, status=status.HTTP_201_CREATED)
             else:
+                if new_resume_section:
+                    new_resume_section.delete()
                 return ErrorResponse(
                     code=ErrorCode.INVALID_REQUEST, message=f'Invalid Data provided!',
                     details=education_ser.errors, extra={'data': request.data}
                 ).response
         if request.method == 'DELETE':
             try:
-                education: Education = Education.objects.get(id=str(education_id).strip())
+                education: Education = authenticated_user.education_set.get(
+                    id=str(education_id).strip(), resume_section__resume=resume
+                )
+                resume_sec = education.resume_section
                 education.delete()
+                resume_sec.delete()
                 return Response(status=status.HTTP_204_NO_CONTENT)
             except Education.DoesNotExist:
                 return ErrorResponse(code=ErrorCode.NOT_FOUND, message=f'Education not found!',
@@ -300,12 +317,19 @@ def education_crud(request, user_id: str, resume_id: str, education_id: str | No
     description="Deletes an experience from the user's resume as specified in the resume id."
 )
 @api_view(['GET', 'POST', 'DELETE'])
-def experience_crud(request, user_id: str, resume_id: str, experience_id: str | None = None):
+def experience_crud(request, resume_id: str, experience_id: str | None = None):
     try:
+        # Ownership Check for all types of API
+        authenticated_user: User = request.user
+        if not authenticated_user.resume_set.filter(id=resume_id).exists():
+            return ErrorResponse(code=ErrorCode.NOT_FOUND, message='Resume not found!', status_code=404).response
+        resume: Resume = authenticated_user.resume_set.get(id=resume_id)
+
+        # Main APIs Logic
         if request.method == 'GET':
             if experience_id:
-                experience_qs: QuerySet[Experience] = Experience.objects.filter(
-                    user__id=user_id, resume_section__resume__id=resume_id
+                experience_qs: QuerySet[Experience] = authenticated_user.experience_set.filter(
+                    resume_section__resume=resume
                 )
                 if experience_qs.exists():
                     return Response(ExperienceFullSerializer(experience_qs.first()).data)
@@ -315,26 +339,37 @@ def experience_crud(request, user_id: str, resume_id: str, experience_id: str | 
                     ).response
             else:  # If resume id is not provided send all resumes for the user
                 return Response(
-                    ExperienceFullSerializer(Experience.objects.filter(
-                        user__id=user_id, resume_section__resume__id=resume_id), many=True).data
-                )
+                    ExperienceFullSerializer(
+                        authenticated_user.experience_set.filter(resume_section__resume=resume), many=True).data
+                    )
         if request.method == 'POST':
             if experience_id:
                 return ErrorResponse(code=ErrorCode.INVALID_REQUEST,
                                      message='Experience Id i not allowed in create request!').response
+            new_resume_section = resume.create_section(section_type=ResumeSection.ResumeSectionType.Education)
+            payload = request.data
+            payload['user'] = authenticated_user.id
+            payload['resume_section'] = new_resume_section.id
             experience_ser: ExperienceUpsertSerializer = ExperienceUpsertSerializer(data=request.data)
             if experience_ser.is_valid():
                 new_experience = experience_ser.save()
                 return Response(ExperienceFullSerializer(new_experience).data, status=status.HTTP_201_CREATED)
             else:
+                if new_resume_section:
+                    new_resume_section.delete()
                 return ErrorResponse(
                     code=ErrorCode.INVALID_REQUEST, message=f'Invalid Data provided!',
                     details=experience_ser.errors, extra={'data': request.data}
                 ).response
         if request.method == 'DELETE':
             try:
-                experience: Experience = Experience.objects.get(id=str(experience_id).strip())
+                experience: Experience = authenticated_user.experience_set.get(
+                    id=str(experience_id).strip(), resume_section__resume=resume
+                )
+                resume_sec = experience.resume_section
                 experience.delete()
+                resume_sec.delete()
+
                 return Response(status=status.HTTP_204_NO_CONTENT)
             except Education.DoesNotExist:
                 return ErrorResponse(code=ErrorCode.NOT_FOUND, message=f'Experience not found!',
--- RESUME/models.py
+++ RESUME/models.py
@@ -23,11 +23,20 @@ class Resume(models.Model):
         help_text='The unique identifier for the resume entry.'
     )
     user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='The user who the resume belongs to.')
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, blank=True, null=True, help_text='The job the resume is for. (optional in case it\'s a base resume for the user.)')
-    base = models.BooleanField(default=False, help_text='Whether the resume is a base resume for the user. One user can ')
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, blank=True, null=True,
+                            help_text='The job the resume is for. (optional in case it\'s a base resume for the user.)')
+    base = models.BooleanField(default=False,
+                               help_text='Whether the resume is a base resume for the user. One user can ')
 
     class Meta:
         unique_together = ('user_id', 'job_id')
+
+    def create_section(self, section_type):
+        next_resume_section = self.resumesection_set.all().order_by('-index').first().index + 1 if self.resumesection_set.exists() else 0
+        new_resume_section = ResumeSection.objects.create(
+            resume_id=self.id, type=section_type, index=next_resume_section
+        )
+        return new_resume_section
 
     def __str__(self):
         return f'{self.id} [{self.user.get_full_name()}]'
@@ -46,8 +55,10 @@ class ResumeSection(models.Model):
         help_text='The unique identifier for the resume section entry.'
     )
     resume = models.ForeignKey(Resume, on_delete=models.CASCADE, help_text='The resume the section belongs to.')
-    index = models.IntegerField(help_text='The index of the section in the resume. This number determines the position of the section in the resume. (0-based)')
-    type = models.CharField(max_length=3, choices=ResumeSectionType.choices, help_text='The type of the section. Can be Education, Experience, Skill, Strength, Project or Others.')
+    index = models.IntegerField(
+        help_text='The index of the section in the resume. This number determines the position of the section in the resume. (0-based)')
+    type = models.CharField(max_length=3, choices=ResumeSectionType.choices,
+                            help_text='The type of the section. Can be Education, Experience, Skill, Strength, Project or Others.')
 
     class Meta:
         unique_together = ('resume', 'index')
@@ -65,21 +76,31 @@ class ResumeSection(models.Model):
 
 
 class Education(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text='The unique identifier for the education entry.')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
+                          help_text='The unique identifier for the education entry.')
     user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='The user who the education entry belongs to.')
-    resume_section = models.OneToOneField(ResumeSection, on_delete=models.CASCADE, help_text='The resume section the education entry belongs to.')
+    resume_section = models.OneToOneField(ResumeSection, on_delete=models.CASCADE,
+                                          help_text='The resume section the education entry belongs to.')
     institution_name = models.CharField(max_length=250, help_text='The name of the institution the user studied at.')
     field_of_study = models.CharField(max_length=250, help_text='The field of study the user studied.')
-    degree = models.CharField(max_length=250, null=True, blank=True, help_text='The degree the user obtained. (optional)')
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True, help_text='The country the institution is located in. (optional)')
-    started_from_month = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The month the user started studying. (optional)')
-    started_from_year = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The year the user started studying. (optional)')
-    finished_at_month = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The month the user finished studying. (optional)')
-    finished_at_year = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The year the user finished studying. (optional)')
+    degree = models.CharField(max_length=250, null=True, blank=True,
+                              help_text='The degree the user obtained. (optional)')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, blank=True,
+                                help_text='The country the institution is located in. (optional)')
+    started_from_month = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                          help_text='The month the user started studying. (optional)')
+    started_from_year = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                         help_text='The year the user started studying. (optional)')
+    finished_at_month = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                         help_text='The month the user finished studying. (optional)')
+    finished_at_year = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                        help_text='The year the user finished studying. (optional)')
     current = models.BooleanField(default=False, help_text='Whether the user is currently studying. default: False')
-    description = models.TextField(max_length=3000, null=True, blank=True, help_text='The description of the education entry. User can provide any kind of description for that user. Usually in HTML format to support rich text. (optional)')  # TODO: May need to change the length
+    description = models.TextField(max_length=3000, null=True, blank=True,
+                                   help_text='The description of the education entry. User can provide any kind of description for that user. Usually in HTML format to support rich text. (optional)')  # TODO: May need to change the length
     created_at = models.DateTimeField(auto_now_add=True, help_text='The date and time the education entry was created.')
-    updated_at = models.DateTimeField(auto_now=True, help_text='The date and time the education entry was last updated.')
+    updated_at = models.DateTimeField(auto_now=True,
+                                      help_text='The date and time the education entry was last updated.')
 
     def __str__(self):
         return f'{self.institution_name}'
@@ -96,22 +117,33 @@ class Experience(models.Model):
         VOLUNTEER = 'vol'
         TRAINEE = 'tra'
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text='The unique identifier for the experience entry.')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False,
+                          help_text='The unique identifier for the experience entry.')
     user = models.ForeignKey(User, on_delete=models.CASCADE, help_text='The user who the experience entry belongs to.')
     resume_section = models.OneToOneField(ResumeSection, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=250, help_text='The name of the company the user worked at.')
     job_title = models.CharField(max_length=250, help_text='The title of the job the user had.')
-    employment_type = models.CharField(max_length=3, choices=EmploymentType.choices, help_text='The type of employment the user had. Can be Full Time, Part Time, Contract, Internship, Freelance, Self Employed, Volunteer or Trainee.')
-    city = models.CharField(max_length=50, blank=True, null=True, help_text='The city the company is located in. (optional)')
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True, help_text='The country the company is located in. (optional)')
-    started_from_month = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The month the user started working.')
-    started_from_year = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The year the user started working.')
-    finished_at_month = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The month the user finished working. (optional)')
-    finished_at_year = models.PositiveSmallIntegerField(null=True, blank=True, help_text='The year the user finished working. (optional)')
+    employment_type = models.CharField(max_length=3, choices=EmploymentType.choices,
+                                       help_text='The type of employment the user had. Can be Full Time, Part Time, Contract, Internship, Freelance, Self Employed, Volunteer or Trainee.')
+    city = models.CharField(max_length=50, blank=True, null=True,
+                            help_text='The city the company is located in. (optional)')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True,
+                                help_text='The country the company is located in. (optional)')
+    started_from_month = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                          help_text='The month the user started working.')
+    started_from_year = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                         help_text='The year the user started working.')
+    finished_at_month = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                         help_text='The month the user finished working. (optional)')
+    finished_at_year = models.PositiveSmallIntegerField(null=True, blank=True,
+                                                        help_text='The year the user finished working. (optional)')
     current = models.BooleanField(default=False, help_text='Whether the user is currently working. default: False')
-    description = models.TextField(max_length=3000, null=True, blank=True, help_text='The description of the experience entry. User can provide any kind of description for that user. Usually in HTML format to support rich text. (optional)')  # TODO: May need to change the length
-    created_at = models.DateTimeField(auto_now_add=True, help_text='The date and time the experience entry was created.')
-    updated_at = models.DateTimeField(auto_now=True, help_text='The date and time the experience entry was last updated.')
+    description = models.TextField(max_length=3000, null=True, blank=True,
+                                   help_text='The description of the experience entry. User can provide any kind of description for that user. Usually in HTML format to support rich text. (optional)')  # TODO: May need to change the length
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      help_text='The date and time the experience entry was created.')
+    updated_at = models.DateTimeField(auto_now=True,
+                                      help_text='The date and time the experience entry was last updated.')
 
     def __str__(self):
         return f'{self.company_name}'
