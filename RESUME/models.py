77e1da61088f8faa1bf08910822e4943960632c9
Arnab Biswas <ping.arnab@gmail.com>
2025-01-28 23:29:25 +0530

    core: change resume schema to accommodated resume versions

--- RESUME/models.py
+++ RESUME/models.py
@@ -13,7 +13,6 @@ def generate_resume_id():
         nanoid = generate_nanoid()
     return f'rsm_{nanoid}'
 
-
 class Resume(models.Model):
     id = models.CharField(
         max_length=25,
@@ -27,16 +26,35 @@ class Resume(models.Model):
                             help_text='The job the resume is for. (optional in case it\'s a base resume for the user.)')
     base = models.BooleanField(default=False,
                                help_text='Whether the resume is a base resume for the user. One user can ')
+    variations = models.ManyToManyField('Resume', related_name='related_resumes', blank=True, null=True)
+    version = models.IntegerField(default=1, help_text='The version of the resume.')
 
     class Meta:
-        unique_together = ('user_id', 'job_id')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'job', 'version'],
+                name="unique_resume_per_job",
+                violation_error_message="This job is already have a resume.",
+            ),
+            models.UniqueConstraint(
+                fields=['user', 'base', 'version'],
+                name="unique_base_resume",
+                violation_error_message="Base resume already exists.",
+            ),
+        ]
 
     def create_section(self, section_type):
-        next_resume_section = self.resumesection_set.all().order_by('-index').first().index + 1 if self.resumesection_set.exists() else 0
+        next_resume_section = self.resumesection_set.all().order_by(
+            '-index').first().index + 1 if self.resumesection_set.exists() else 0
         new_resume_section = ResumeSection.objects.create(
             resume_id=self.id, type=section_type, index=next_resume_section
         )
         return new_resume_section
+
+    def generate_next_resume_version(self):
+        if self.variations.exists():
+            return self.variations.order_by('version').last().version + 1
+        return 1
 
     def __str__(self):
         return f'{self.id} [{self.user.get_full_name()}]'
